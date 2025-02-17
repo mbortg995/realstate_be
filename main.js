2c03b2e027719120c751344b5bca33a833e18739
mbortg995 <mbortg995@gmail.com>
2025-02-17 20:10:34 +0100

    feat: created router

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { connectToDatabase } from './database.js';
+import router from './src/api/router.js';
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
-app.use(express.static('public'));
+app.use('/api', router);
+
 
 connectToDatabase();
 
